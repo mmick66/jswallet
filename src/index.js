f0659ec2044eed483da55f2c533b337b617b602c
Michael Michailidis <mmick66@gmail.com>
2020-05-29 10:47:33 +0300

    Updated package version with critical issue

--- src/index.js
+++ src/index.js
@@ -16,6 +16,9 @@ const createWindow = async () => {
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
 
     // and load the index.html of the app.
